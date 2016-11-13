a3ce04dbae2090f58840964dd72cfc5e78b80cce
bruvlee <lee.lichtsinn@gmail.com>
2016-11-13 14:10:15 -0500

    Imported redux and react-redux into client.jsx

--- client.jsx
+++ client.jsx
@@ -2,7 +2,11 @@ console.log('client.js');
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Link, Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reducer from './reducers/index';
+// import action from './actions';
 
 import App from './components/App/App';
 import Home from './components/Home/Home';
@@ -12,7 +16,14 @@ import NavBar from './components/NavBar/NavBar';
 import MessageList from './components/MessageList/MessageList';
 import Message from './components/Message/Message';
 
-ReactDom.render((
+/* eslint-disable no-underscore-dangle */
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-enable */
+
+const render = () => ReactDom.render((
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
@@ -21,6 +32,10 @@ ReactDom.render((
         <Route path="preferences" component={UserPrefs} />
       </Route>
     </Router>
+  </Provider>
   ),
   document.getElementById('root')
 );
+
+render();
+store.subscribe(render);
