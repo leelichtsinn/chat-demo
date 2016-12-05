43400ee7031dca54cb80f8ab4409e3f31e526e59
bruvlee <lee.lichtsinn@gmail.com>
2016-12-04 21:48:31 -0500

    WIP

--- server.babel.js
+++ server.babel.js
@@ -6,12 +6,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const app = express();
+app.use(passport.initialize());
+require('./strategies/passport-local')(passport);
+
 const index = require('./routes/index');
 const users = require('./routes/users');
-require('./strategies/passport-local')(passport);
 const userRoutes = require('./routes/user')(passport);
-
-const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,10 +25,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
+
 
 app.use('/api', index);
-app.use('/api/users', users);
+// app.use('/api/users', users);
 app.use('/api/user', userRoutes);
 
 app.route(/.*/).get(function(req, res) {
