ea54562a5b5bf54943e65dcef0e3ee81359624e0
bruvlee <lee.lichtsinn@gmail.com>
2017-01-13 13:08:50 -0500

    minor error fixes

--- client.jsx
+++ client.jsx
@@ -3,7 +3,6 @@ console.log('client.js');
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { Link, Router, Route, IndexRoute, browserHistory } from 'react-router';
 import history from 'history';
 import store from './store';
--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 import NavBar from '../NavBar/NavBar';
 
-export default class User extends Component {
+export default class Profile extends Component {
   constructor(props) {
     super(props);
   }
@@ -26,7 +26,7 @@ export default class User extends Component {
   }
 }
 
-// User.propTypes = {
-//   userDetails: Proptypes.object,
-//   onDidMount: PropTypes.func
-// }
+Profile.propTypes = {
+  userDetails: PropTypes.object,
+  onDidMount: PropTypes.func
+}
