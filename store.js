5d74b5e30777a24910ec35adcf5db596f181b52f
bruvlee <lee.lichtsinn@gmail.com>
2017-01-15 21:30:56 -0500

    added redux devtools extension to store.js

--- store.js
+++ store.js
@@ -2,6 +2,10 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-disable */
 
 export default store;
